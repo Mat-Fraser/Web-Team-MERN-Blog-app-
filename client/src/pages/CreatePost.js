70ba5f2def7b94a42a63b8f0e36f745fcc766d74
Mat-Fraser <86045021+Mat-Fraser@users.noreply.github.com>
2023-04-13 15:20:09 +0530

    Update CreatePost.js

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -38,7 +38,7 @@ function CreatePost() {
     data.set('content', content);
     data.set('file', files[0]);
     ev.preventDefault();
-    const response = await  fetch('http://localhost:4000/post', {
+    const response = await  fetch('https://webteams.onrender.com/post', {
       method: 'POST',
       body: data,
       credentials:'include',

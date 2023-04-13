ff956bf96b97dbcb3790b327c73e2b30e9317716
Mat-Fraser <86045021+Mat-Fraser@users.noreply.github.com>
2023-04-13 15:45:03 +0530

    Update Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -5,7 +5,7 @@ import {UserContext} from "./UserContext";
 export default function Header(){
   const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect(() => {
-    fetch('https://webteams.onrender.com/profile', {
+    fetch('https://webteam.onrender.com/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
@@ -15,7 +15,7 @@ export default function Header(){
   }, []);
 
   function logout() {
-    fetch('https://webteams.onrender.com/logout', {
+    fetch('https://webteam.onrender.com/logout', {
       credentials: 'include',
       method: 'POST',
     });
